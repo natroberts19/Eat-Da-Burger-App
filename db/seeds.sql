13b5b3edffeaa1eaafa143fc6daebb6c73eef4d7
Natalie Roberts <nr_roberts@yahoo.com>
2018-02-03 15:21:53 -0500

    app and db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Nothingburger');
+INSERT INTO burgers (burger_name) VALUES ('Burger Bar');
+INSERT INTO burgers (burger_name) VALUES ('Stuffed Double Burger');
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(".burger/public"));
+
+// parse application/x-www-form-urlencoded. Change extended to "true."
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
