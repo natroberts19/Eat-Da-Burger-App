501f2597f777174d5473fcbef4f7615307adb63b
Natalie Roberts <nr_roberts@yahoo.com>
2018-02-04 15:22:16 -0500

    changes to handlebars files

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var cat = {
+var burger = {
   selectAll: function(cb) {
     orm.selectAll("burgers", function(res) {
       cb(res);
@@ -20,5 +20,5 @@ var cat = {
   },
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgers_controller.js).
 module.exports = burger;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var path = require("path");
+var nodemon = require("nodemon");
 
 var PORT = process.env.PORT || 3000;
 
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(".burger/public"));
+app.use(express.static(path.join(__dirname,"/burger/public")));
 
 // parse application/x-www-form-urlencoded. Change extended to "true."
 app.use(bodyParser.urlencoded({ extended: true }));
