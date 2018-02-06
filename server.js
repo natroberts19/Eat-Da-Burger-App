7b303e6fff64249544e8e29c572584f444b29857
Natalie Roberts <nr_roberts@yahoo.com>
2018-02-06 18:08:18 -0500

    added fix for heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var path = require("path");
-var nodemon = require("nodemon");
 
 var PORT = process.env.PORT || 3000;
 
